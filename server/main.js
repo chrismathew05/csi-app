fc9bf875c4acef0d55c83700438870907ebf6a14
chrismathew05 <cmathew@edu.uwaterloo.ca>
2018-08-30 20:13:36 -0400

    Fixed local storage for images

--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ import Images from '../imports/api/Images.js';
 
 Meteor.startup(() => {
     // code to run on server at startup
+    console.log(Meteor.rootPath);
 
     // create regular and admin users
     if (!Accounts.findUserByUsername(Meteor.settings.private.userId)) {
